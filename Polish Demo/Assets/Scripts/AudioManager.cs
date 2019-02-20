06b0a09e8e97784a0f2e02a3430c0cc8106bce71
ICS 161 <eaho1@uci.edu>
2019-02-19 20:31:43 -0800

    Added Audio Manager

---
+++ Polish Demo/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+    public AudioSource m_BGMPlayer, m_SFXPlayer;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void PlaySFX(string clip)
+    {
+        //m_SFXPlayer.PlayOneShot(clip, Random.Range(0.5f, 1f));
+        PlaySFX(clip, Random.Range(0.5f, 1f));
+    }
+
+    public void PlaySFX(string clip, float volume)
+    {
+        AudioClip newClip = Resources.Load($"Audio/{clip}") as AudioClip;
+        //AudioClip newClip = Resources.Load<AudioClip>($"Audio/{clip}");
+        m_SFXPlayer.PlayOneShot(newClip, volume);
+    }
+}
---
+++ Polish Demo/Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //AudioSource m_audioSource;
+
+    //void Awake()
+    //{
+    //    m_audioSource = this.GetComponent<AudioSource>();
+    //}
+
+    //public AudioClip shootSFX;
+    //public AudioClip jumpSFX;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Shoot();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    void Shoot()
+    {
+        //m_audioSource.Play();
+        AudioManager.instance.PlaySFX("Shoot");
+    }
+
+    void Jump()
+    {
+        AudioManager.instance.PlaySFX("Jump");
+    }
+}
